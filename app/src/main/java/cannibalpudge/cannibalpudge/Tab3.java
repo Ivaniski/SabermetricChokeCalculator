1fa405e7cf518b46238b0ee6aace5bf5581ecb9a
ECSanchez <ecsanchez97@gmail.com>
2018-01-04 19:50:04 -0800

    made shit work

---
+++ app/src/main/java/cannibalpudge/cannibalpudge/Tab3.java
@@ -0,0 +1,36 @@
+package cannibalpudge.cannibalpudge;
+
+/**
+ * Created by Eddy on 1/4/2018.
+ */
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+
+public class Tab3 extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        //textView.setText(getString(R.string.section_format, new Object[]{getArguments().getInt(ARG_SECTION_NUMBER)}));
+        return rootView;
+    }
+}
